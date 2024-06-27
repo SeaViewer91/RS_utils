544ec86a0464325e373691bbe986d67c0cd20582
Suho Bak <67940301+SeaViewer91@users.noreply.github.com>
2024-06-27 16:17:12 +0900

    Update video_merge.py

--- video_merge/video_merge.py
+++ video_merge/video_merge.py
@@ -10,8 +10,8 @@ import datetime
 
 def concatenate_mov_to_mp4_gpu(input_directory, output_filename):
     # Step 1: Get list of mov files sorted by index
-    mov_files = sorted([f for f in os.listdir(input_directory) if f.lower().endswith('.mov')],
-                       key=lambda x: int(x.split('_')[-1].split('.')[0]))
+    mov_files = sorted([f for f in os.listdir(input_directory) if f.lower().endswith('.mov')], # 디렉토리 내에 저장된 mov 파일의 리스트 생성
+                       key=lambda x: int(x.split('_')[-1].split('.')[0])) # 동영상 파일명이 'DJI_0001.mov'와 같이 저장되어 있으므로, 언더바 이후 숫자를 기준으로 파일 리스트를 재정렬
     
     # Step 2: Create a temporary file list for FFmpeg
     file_list_path = os.path.join(input_directory, 'file_list.txt')
@@ -26,7 +26,7 @@ def concatenate_mov_to_mp4_gpu(input_directory, output_filename):
         '-f', 'concat',
         '-safe', '0',
         '-i', file_list_path,
-        '-c:v', 'h264_nvenc',
+        '-c:v', 'h264_nvenc',  # 그래픽 가속을 통해 h.264 mp4로 export
         '-vf', 'scale=1920:1080',
         '-r', '24',
         '-pix_fmt', 'yuv420p',
@@ -47,7 +47,18 @@ def main():
     root_dir_path = 'D:/video/samples'
     
     dirs = [d for d in os.listdir(root_dir_path) if os.path.isdir(os.path.join(root_dir_path, d))]
-    
+
+    '''
+    디렉토리 구조는 아래와 같아야 함.
+
+    root_dir
+    ㄴset1
+      ㄴDJI_0001.mov
+      ㄴDJI_0002.mov
+      ㄴDJI_0003.mov
+    ㄴset2
+    ㄴset3
+    '''
     for directory_name in dirs:
         concatenate_mov_to_mp4_gpu(
             input_directory=os.path.join(root_dir_path, directory_name), 
